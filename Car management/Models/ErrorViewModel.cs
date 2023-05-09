987f373159fc4a276ae8c43b230eada4bbc1d4ba
dev1@flairminds.com <dev1@flairminds.com>
2023-05-10 00:20:58 +0530

    Let's Start your journey

---
+++ Car management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Car_management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
